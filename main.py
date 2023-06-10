7efa76df9ef36ba3bbfabe49785229dd2035cbf1
AMAQSA <132121784+AMAQSA@users.noreply.github.com>
2023-06-10 12:17:48 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,45 +1,37 @@
-
-from settings import settings
 import discord
-# import * - adalah cara cepat untuk mengimpor semua file di perpustakaan
-from bot_logic import *
+from discord.ext import commands
+import os
+import random
+import requests
 
-# Variabel intents menyimpan hak istimewa bot
 intents = discord.Intents.default()
-# Mengaktifkan hak istimewa message-reading
 intents.message_content = True
-# Membuat bot di variabel klien dan memindahkan hak istimewa
-client = discord.Client(intents=intents)
 
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-# Setelah bot siap, ia akan mencetak namanya!
-@client.event
+# Dan inilah cara Kamu mengganti nama file dari variabel!
+sampah_anorganik =  ["plastik","besi","baja","emas"]
+sampah_organik =  ["nasi padang","ayam","kayu","daun"]
+# sampah = ["organik" , "anorganik"]
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-
-# Saat bot menerima pesan, bot akan mengirimkan pesan di saluran yang sama!
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Saya! Saya bot!')
-    elif message.content.startswith('$smile'):
-        await message.channel.send(gen_emodji())
-    elif message.content.startswith('$coin'):
-        await message.channel.send(flip_coin())
-    elif message.content.startswith('$pass'):
-        await message.channel.send(gen_pass(10))
-    elif message.content.startswith('$emoji1'):
-        await message.channel.send('-_-') 
-    elif message.content.startswith('$kamusiapa?'):
-        await message.channel.send('Saya bot')
-    elif message.content.startswith('$help'):
-        await message.channel.send('kamu bisa memerintahkan dalam beberapa macam perintah \nmisalnya : \n 1.$hello \n 2.$smile \n 3.$coin \n 4.$pass \n 5.$emoji1 \n 6.$kamusiapa? \n 7.$help \n 8.$rank')
-    elif message.content.startswith('$rank'):
-        await message.channel.send('grandmaster ml , ff heroic')
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+        
+@bot.command()
+async def pilih(ctx , sampah):
+        if sampah in sampah_organik:
+            await ctx.send("masukkan ke sampah sampah organik")
     
 
-
-client.run(settings["TOKEN"])
+        if sampah in sampah_anorganik:
+            await ctx.send("masukkan ke sampah sampah anorganik")
+   
+    # await ctx.send("nama sampah")
+    # if msg.content in sampah_organik:
+    #     await ctx.send("sampah oraganik")
+bot.run("MTEwNjgwNTAxMDI5NTE3MzE3MA.Gpygzb.eVjkyiBgc5pmUBfTLR2Y8buACTOnotFuYjO1cM")
